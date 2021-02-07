480cf5326ab3e7011668670403a24ed1db20f117
B.simak <b.simak@gmail.com>
2021-02-07 22:35:43 +0100

    Initial commit

---
+++ src/main/java/org/example/MavenTestApp.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.h2.jdbcx.JdbcConnectionPool;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class MavenTestApp {
+    public static void main (String [] args) throws IOException {
+        try (InputStream is = MavenTestApp.class.getResourceAsStream("/application.properties")) {
+            Properties p = new Properties();
+            p.load(is);
+            System.out.println(p);
+        }
+
+        JdbcConnectionPool cp = JdbcConnectionPool.create("jdbc:h2:mem:", "sa", "sa");
+        try (Connection conn = cp.getConnection()) {
+            System.out.println("Is my database connection valid: " + conn.isValid(0));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
